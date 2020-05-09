37c40d592483579def93ca3a803909cce294a5d8
FailCake <eduardo.fernandes.work@gmail.com>
2020-05-09 17:33:09 +0200

    a

---
+++ build/precache-manifest.72a53bd12f0d0dc896bcefcfa9214044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009b445d70f076e0903cdfd8ee0a699e",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "4a7c4b4b7374bde20552",
+    "url": "/horizoverlay/static/css/main.b0a6984b.chunk.css"
+  },
+  {
+    "revision": "7a2f89d10b511b9c0d33",
+    "url": "/horizoverlay/static/js/2.b7868ab6.chunk.js"
+  },
+  {
+    "revision": "4a7c4b4b7374bde20552",
+    "url": "/horizoverlay/static/js/main.c7516849.chunk.js"
+  },
+  {
+    "revision": "f5cff2eb4effd23c272c",
+    "url": "/horizoverlay/static/js/runtime~main.5766f4f7.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  }
+]);
